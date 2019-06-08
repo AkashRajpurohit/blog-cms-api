65ce0d89a31c879f2ae85179b8284b5fe16ba3cf
Akash Rajpurohit <akash009.dev@gmail.com>
2019-06-08 20:12:17 +0530

    :sparkles: blogs model

---
+++ src/api/blog/blog.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const BlogSchema = new Schema(
+  {
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'users',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    published: {
+      type: Boolean,
+      required: true
+    }
+  },
+  { timestamps: true }
+)
+
+module.exports = User = mongoose.model('users', BlogSchema)
