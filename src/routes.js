8a1af63820d44957747f35a09921a0755959788f
Akash Rajpurohit <akash009.dev@gmail.com>
2019-06-08 20:14:10 +0530

    bootstraped blogs routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
+
 const user = require('./api/user/user.routes')
+const blog = require('./api/blog/blog.routes')
 
 router.use('/user', user)
+router.use('/blog', blog)
 
 module.exports = router
