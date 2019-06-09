e1749b38e542d76849e24223aea699cce726bebe
Akash Rajpurohit <akash009.dev@gmail.com>
2019-06-09 21:13:57 +0530

    :boom: isAdmin Middleware

---
+++ src/utils/isAdmin.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken')
+
+const { secret } = require('../config')
+// Error response
+const errorResponse = require('./errorResponse')
+// Constants
+const constants = require('./constants')
+
+module.exports = async (req, res, next) => {
+  const bearerToken = req.headers['authorization']
+  if (bearerToken) {
+    const token = bearerToken.split(' ')[1]
+    try {
+      const { userId, usertype } = await jwt.verify(token, secret)
+
+      if (usertype !== 'admin') {
+        return res.status(401).json(errorResponse(constants.UNAUTHORIZED))
+      }
+
+      req.userId = userId
+
+      // if everything works fine, go to next middleware
+      next()
+    } catch (err) {
+      return res.status(400).json(errorResponse(constants.INVALID_TOKEN))
+    }
+  } else {
+    res.status(400).json(errorResponse(constants.NO_TOKEN))
+  }
+}
