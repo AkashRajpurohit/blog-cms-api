4ce4c25067e7963a102992b529bb2c9736571e2c
Akash Rajpurohit <akash009.dev@gmail.com>
2019-06-09 21:12:07 +0530

    :tada: admin routes initialized

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const user = require('./api/user/user.routes')
 const blog = require('./api/blog/blog.routes')
+const admin = require('./api/admin/admin.routes')
 
 router.use('/user', user)
 router.use('/blog', blog)
+router.use('/admin', admin)
 
 module.exports = router
