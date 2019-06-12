208a823e55cbd076c072c175bb96f9760b764650
Akash Rajpurohit <akash009.dev@gmail.com>
2019-06-12 18:54:11 +0530

    :tada: docs route added; show msg in / route

--- src/app.js
+++ src/app.js
@@ -10,7 +10,14 @@ const app = express()
 app.use(...commonMiddlewares)
 
 app.get('/', (_, res) => {
-  res.json({ message: 'Hello World!' })
+  res.json({
+    message: 'Hello World!',
+    'where to?': 'Read the docs at: /api/v1/docs',
+  })
+})
+
+app.get('/api/v1/docs', (_, res) => {
+  res.redirect('https://documenter.getpostman.com/view/4346639/S1TZzbts')
 })
 
 app.use('/api/v1', routes)
