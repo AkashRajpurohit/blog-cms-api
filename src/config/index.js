457142814a86ab3bafe1e8161ad2067b00dee23e
Akash Rajpurohit <akash009.dev@gmail.com>
2019-07-11 14:04:38 +0530

    :sparkles: dotenv config for env variables

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
+
 module.exports = {
-  mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/blog-cms',
-  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET || 'djksahchjasbdjhcsbcashichij38927rychkjdbfkjs',
-  refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET || 'cdbsc782hf932hdo2nc02j308hbi42h98h2cn82j024f0203',
-  baseUrl: process.env.BASE_URL || 'http://localhost:4000'
+  mongoURI: process.env.MONGO_URI,
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+  refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
+  baseUrl: process.env.BASE_URL,
+  gmailEmail: process.env.GMAIL_EMAIL,
+  gmailPassword: process.env.GMAIL_PASSWORD
 }
